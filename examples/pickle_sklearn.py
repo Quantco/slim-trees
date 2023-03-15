e8c55e02d8568f382a03e9d623e84a103b9cb21e
Pavel Zwerschke <pavelzw@gmail.com>
2023-03-15 16:23:49 +0100

    WIP

--- examples/pickle_sklearn.py
+++ examples/pickle_sklearn.py
@@ -2,6 +2,8 @@ import pathlib
 import tempfile
 
 from sklearn.ensemble import RandomForestRegressor
+
+from benchmark import train_model_sklearn
 from utils import (
     evaluate_compression_performance,
     evaluate_prediction_difference,
@@ -9,7 +11,7 @@ from utils import (
 )
 
 from slim_trees import dump_sklearn_compressed
-from slim_trees.pickling import load_compressed
+from slim_trees.pickling import load_compressed, get_pickled_size
 from slim_trees.sklearn_tree import dump_sklearn
 
 
@@ -21,7 +23,13 @@ def train_model() -> RandomForestRegressor:
 
 
 if __name__ == "__main__":
-    model = train_model()
+    model = train_model_sklearn()
+    # model = train_model()
+
+    print(f"No compress: {get_pickled_size(model, compression='no') / (2 ** 20)} MB")
+    # print(f"No compress lzma: {get_pickled_size(model, compression='lzma') / (2 ** 20)} MB")
+    print(f"compress old: {get_pickled_size(model, compression='no', dump_function=dump_sklearn) / (2 ** 20)} MB")
+    print(f"compress old lzma: {get_pickled_size(model, compression='lzma', dump_function=dump_sklearn) / (2 ** 20)} MB")
 
     with tempfile.TemporaryDirectory() as tmpdir:
         path = pathlib.Path(tmpdir) / "model.pkl"
