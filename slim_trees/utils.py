8a9fa021016a83c0586d19caf1d4abbb0337b958
Pavel Zwerschke <pavelzw@gmail.com>
2023-03-16 17:38:57 +0100

    Add version warnings

---
+++ slim_trees/utils.py
@@ -0,0 +1,11 @@
+import warnings
+
+from slim_trees import __version__ as slim_trees_version
+
+
+def check_version(version: str):
+    if version != slim_trees_version:
+        warnings.warn(
+            f"Version mismatch: slim_trees version {slim_trees_version} "
+            f"does not match version {version} of the model."
+        )
