0c35448068538ef8ad869754bc232bbf75464ddc
Pavel Zwerschke <pavelzw@gmail.com>
2023-05-31 10:58:42 +0200

    cleanup

--- .github/scripts/read_version.sh
+++ .github/scripts/read_version.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 # first get line with version_info, then extract thing in parantheses, then replace ", " with "."
-echo $(cat $1 | grep "version_info =" | sed "s/^.*(\([^()]*\)).*$/\1/" | sed "s/, /./g")
+grep "version_info =" | sed "s/^.*(\([^()]*\)).*$/\1/" | sed "s/, /./g"
